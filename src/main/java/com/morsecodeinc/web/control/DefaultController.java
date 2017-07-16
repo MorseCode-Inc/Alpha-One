50adcec6b09526fce20f8939dce5eb2b14b05a38
Brad <brad@morsecode-inc.com>
2017-07-16 14:35:39 -0600

    addining default controller and raw view.

---
+++ src/main/java/com/morsecodeinc/web/control/DefaultController.java
@@ -0,0 +1,12 @@
+package com.morsecodeinc.web.control;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by morsecode on 7/16/2017.
+ */
+@RestController
+public class DefaultController {
+
+
+}
---
+++ src/main/java/com/morsecodeinc/web/view/RawView.java
@@ -0,0 +1,10 @@
+package com.morsecodeinc.web.view;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by morsecode on 7/16/2017.
+ */
+@ResponseBody
+public class RawView {
+}
