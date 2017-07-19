9b9887fd166a68250678a20ce182f020101a80e0
Brad <brad@morsecode-inc.com>
2017-07-18 19:31:49 -0600

    error controller.

--- src/main/java/com/morsecodeinc/web/control/Errors.java
+++ src/main/java/com/morsecodeinc/web/control/Errors.java
@@ -1,7 +1,39 @@
 package com.morsecodeinc.web.control;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by morsecode on 7/18/2017.
  */
+@RestControllerAdvice
 public class Errors {
+
+    private final Logger LOG= LoggerFactory.getLogger(Errors.class);
+
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleBadRequest(HttpRequest request) {
+        LOG.error("Got a Bad Request. ["+ request +"]");
+        Map<String, Object> response= new HashMap<>();
+        return response;
+    }
+
+    @ExceptionHandler({NullPointerException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleNullPointer(HttpRequest request, NullPointerException npe) {
+        LOG.error("Null Pointer. ["+ request +"]", npe);
+        Map<String, Object> response= new HashMap<>();
+        return response;
+    }
+
 }
