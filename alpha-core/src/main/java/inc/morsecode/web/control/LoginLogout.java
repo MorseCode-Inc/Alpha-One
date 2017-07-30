45247a62c7064756b23e3fc74c3475343c9e9c44
Brad <brad@morsecode-inc.com>
2017-07-29 19:33:01 -0600

    roughing out new .jade files for necessary flight record data collection

--- alpha-core/src/main/java/inc/morsecode/web/control/LoginLogout.java
+++ alpha-core/src/main/java/inc/morsecode/web/control/LoginLogout.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginLogout {
@@ -12,7 +13,6 @@ public class LoginLogout {
     @RequestMapping(method= RequestMethod.GET,
             path= "/welcome-login")
     public String welcome() {
-
         // this is the login form page
         return "welcome-login";
     }
@@ -20,8 +20,13 @@ public class LoginLogout {
     @RequestMapping(method= RequestMethod.POST,
             path="/welcome-login")
     public String login(HttpServletRequest httpReq) {
-
         // this is the landing page on successful login.
         return "welcome";
+    }
+
+    @RequestMapping(path="/goodbye")
+    public String goodbye(HttpServletRequest httpReq, HttpSession session) {
+        // destroy any tokens and session information indicating the use is logged in
+        return "redirect:/";
     }
 }
